506ba0fe7bb05eea19abfc722110005e4dd2b1d4
ayahusu <ayahusu2210@gmail.com>
2026-01-13 19:15:13 +0545

    feat: add search functionality for blogs

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('category/', include('blogs.urls')),
-    path('<slug:slug>/', BlogView.blogs, name='blogs')
+    path('<slug:slug>/', BlogView.blogs, name='blogs'),
+    path('blogs/search/', BlogView.search, name='search')
 ] + static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
